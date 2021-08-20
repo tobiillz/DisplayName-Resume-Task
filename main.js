545d439d1f4dfd59f0a292d336038e63fb1bec25
Tobiillz <ayoadeopeoluwa@yahoo.com>
2021-08-20 17:05:28 +0100

    Committing

---
+++ main.js
@@ -0,0 +1,12 @@
+// Function to reset the input box when the reset button is pushed
+// it also resets the output paragraph
+
+function resetFunction(){
+    // const empty = document.getElementById("output-image").value = "" ;
+    document.getElementById("output-images").innerHTML ="";
+}
+
+function nameFunction(){
+    return document.getElementById("output-images").innerHTML =  "Ayoade Opeoluwa David"
+    
+}
